48a202945927c0ae6837062804c0d0e40c12e31a
Enfraktro <101732377+Enfraktro@users.noreply.github.com>
2024-09-26 13:02:33 +0200

    Update Program.cs

--- TCP_Client/Program.cs
+++ TCP_Client/Program.cs
@@ -7,16 +7,24 @@ class Client
 {
     private const int Port = 8888;
     private const string ServerIp = "127.0.0.1";
-
+// задава стойностите на порта и IP адреса на сървъра към, който ще се свържем
     static void Main()
     {
         TcpClient client = new TcpClient(ServerIp, Port);
         Console.WriteLine("Connected to server. Start chatting!");
+        //Създава TCP клиент
 
         NetworkStream stream = client.GetStream();
 
+        // взема потока от данни, който се използва за комуникация между клиента и сървъра
+
         Thread receiveThread = new Thread(ReceiveMessages);
+                //създава нов поток за получаване на съобщения от сървъра
+
         receiveThread.Start(stream);
+
+        // стартира потока за получаване на съобщения
+
 
         while (true)
         {
@@ -24,6 +32,7 @@ class Client
             byte[] buffer = Encoding.ASCII.GetBytes(message);
             stream.Write(buffer, 0, buffer.Length);
         }
+        // безкраен цикъл в, който програмата чете съобщенията въведени от потребителя в конзолата и ги изпраща на сървъра
     }
 
     static void ReceiveMessages(object obj)
@@ -50,5 +59,6 @@ class Client
                 break;
             }
         }
+        // тази функция приема съобщенията от сървъра, криптира ги и ги изкарва на екрана. Ако потока се прекъсне или има грешка функцията излиза от цикъла и спира да приема съобщения.
     }
 }
